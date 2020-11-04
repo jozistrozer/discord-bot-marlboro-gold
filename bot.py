cbbf77ba1575e17e3cb5cc93868f60dd47b658ca
Jože Strožer <jozistrozer13@gmail.com>
2020-11-04 19:21:36 +0100

    metoda za predvajanje zvoka

--- bot.py
+++ bot.py
@@ -1,27 +1,46 @@
 # bot.py
+from discord.ext.commands import Bot
+from dotenv import load_dotenv
 import os
 import discord
-from dotenv import load_dotenv
+import asyncio
+
 import csvKorona
+from mutagen.mp3 import MP3
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-client = discord.Client()
+
+bot = Bot(command_prefix='~')
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(pass_context=True)
+async def okuzeni(ctx):
+    msg = csvKorona.GetCases()
+    await ctx.send(msg)
 
 
-@client.event
-async def on_message(message):
+@bot.command(pass_context=True)
+async def cigaret(ctx):
+    await predvajajZvok("faracajg", ctx)
 
-    if message.content.startswith("-play") or message.content.startswith("!play") or message.content.startswith(";;play"):
-        await message.delete()
 
-    if "kolk je kej okuženih" in message.content or "kolk je blo okuženih" in message.content:
-        msg = csvKorona.GetCases()
-        await message.channel.send(msg)
+@bot.command(pass_context=True)
+async def mackurina(ctx):
+    await predvajajZvok("mackurina", ctx)
 
-client.run(TOKEN)
+
+async def predvajajZvok(source, ctx):
+    connected = ctx.author.voice
+    if connected:
+        voice_player = await connected.channel.connect()
+        voice_player.play(discord.FFmpegPCMAudio(executable="exes\\ffmpeg.exe", source="mp3\\" + source + ".mp3",
+                                                 options="-loglevel panic"))
+
+        server = ctx.message.guild.voice_client
+
+        audio = MP3("mp3\\" + source + ".mp3")
+        await asyncio.sleep(int(audio.info.length))
+        await server.disconnect()
+
+bot.run(TOKEN)
