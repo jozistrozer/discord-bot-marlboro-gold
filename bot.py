11e07eacce6376ab7cd0f9229b3cc2a501a30a5f
Joze Strozer <jozistrozer13@gmail.com>
2020-11-10 17:45:53 +0100

    Dodana funkcija za prikaz statistika za določen dan ':okuzeni [dan.mesec.leto]'

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
-from discord.ext.commands import Bot, CommandNotFound
+from discord.ext.commands import Bot
 from dotenv import load_dotenv
-import discord, os, asyncio, ffmpeg
+import discord, os, asyncio
 from mutagen.mp3 import MP3
 
 import csvKorona
@@ -12,92 +12,115 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bot = Bot(command_prefix=':')
 bot.remove_command("help")
 
+
 @bot.event
 async def on_ready():
-	print("Zalaufu sm se!: " + bot.user.name)
+    print("Zalaufu sm se!: " + bot.user.name)
 
 
 @bot.event
 async def on_command_error(ctx, error):
-	await ctx.send("Ukaz `" + ctx.message.content + "` ne obstaja. Uporabi `:pomoc` ali `:help`.")
-	await ctx.message.add_reaction("⁉️")
+    await ctx.send("Ukaz `" + ctx.message.content + "` ne obstaja. Uporabi `:pomoc` ali `:help`.")
+    await ctx.message.add_reaction("⁉️")
+    print(error)
+    """
+    user = ctx.message.author
+    try:
+        voice_channel = user.voice.channel
+    except AttributeError:
+        voice_channel = None
+    
+    if voice_channel is None:
+        await ctx.send("Za predvajanje zvokov moraš biti povezan na kanal!")
+    else:
+        if isinstance(error, discord.ClientException):
+            await ctx.send("Trenutno predvajam zvok, mal počaki.")
+        else:
+            await ctx.send("Ukaz `" + ctx.message.content + "` ne obstaja. Uporabi `:pomoc` ali `:help`.")
+            await ctx.message.add_reaction("⁉️")
+    """
 
 
 # OKUZENI
 @bot.command(pass_context=True)
-async def okuzeni(ctx):
-	msg = csvKorona.GetCases()
-	await ctx.send(msg)
-	
+async def okuzeni(ctx, arg=None):
+    msg = ""
+    if arg == None:
+        msg = csvKorona.GetCases()
+    else:
+        msg = csvKorona.GetCases(arg)
+
+    await ctx.send(msg)
+
 
 # FARACAJG
 @bot.command(pass_context=True)
 async def cigaret(ctx):
-	await predvajajZvok("faracajg", ctx)
+    await predvajajZvok("faracajg", ctx)
 
 
 # MACKURINA
 @bot.command(pass_context=True)
 async def mackurina(ctx):
-	await predvajajZvok("mackurina", ctx)
-	
+    await predvajajZvok("mackurina", ctx)
+
 
 # MURJA
 @bot.command(pass_context=True)
 async def murja(ctx):
-	await predvajajZvok("murja", ctx)
+    await predvajajZvok("murja", ctx)
 
 
 # POZABI
 @bot.command(pass_context=True, aliases=['ausbahforzingis'])
 async def pozabi(ctx):
-	await predvajajZvok("pozabi", ctx)
+    await predvajajZvok("pozabi", ctx)
 
 
 # JOZEF
 @bot.command(pass_context=True)
 async def jozef(ctx):
-	await predvajajZvok("jozef", ctx)
+    await predvajajZvok("jozef", ctx)
 
 
 # Pomoc
 @bot.command(pass_context=True, aliases=['help'])
 async def pomoc(ctx):
-	await getHelp(ctx)
-	
+    await getHelp(ctx)
+
 
 async def getHelp(ctx):
-	msg = """
-	**Serbus, js sm Marlboro Gold Bot, razvija me pa stropy.**
-	> **KOMANDE**\n
+    msg = """
+    **Serbus, js sm Marlboro Gold Bot, razvija me pa stropy.**
+    > **KOMANDE**\n
 **Predvajanje zvokov** 🔊
 ```:murja
 :mackurina
 :pozabi, :ausbahforzingis
 :jozef```
 **Covid-19** 😷
-```:okuzeni``` 
+```:okuzeni
+:okuzeni [dan.mesec.leto]``` 
 """
-	await ctx.message.add_reaction("✅")
-	await ctx.message.add_reaction("🚬")
-	await ctx.send(msg)
-	
+    await ctx.message.add_reaction("✅")
+    await ctx.message.add_reaction("🚬")
+    await ctx.send(msg)
+
 
 async def predvajajZvok(source, ctx):
-	source = "./mp3/" + source + ".mp3"
-	user = ctx.message.author
-	voice_channel = user.voice.channel
+    source = "./mp3/" + source + ".mp3"
+    user = ctx.message.author
+    voice_channel = user.voice.channel
 
-	if voice_channel is not None:
-		# create StreamPlayer
-		vc = await voice_channel.connect()
+    if voice_channel is not None:
+        # create StreamPlayer
+        vc = await voice_channel.connect()
 
-		vc.play(discord.FFmpegPCMAudio(source))
-		
-		audio = MP3(source)
-		await asyncio.sleep(int(audio.info.length))
-		await vc.disconnect()
-	else:
-		await ctx.send("Nisi povezan na noben kanal. Povež se v en kanal in probi še enkrat")
+        vc.play(discord.FFmpegPCMAudio(source))
+
+        audio = MP3(source)
+        await asyncio.sleep(int(audio.info.length))
+        await vc.disconnect()
+
 
 bot.run(TOKEN)
--- csvKorona.py
+++ csvKorona.py
@@ -2,6 +2,7 @@ import requests
 from contextlib import closing
 import csv
 import datetime
+from datetime import date
 import langDay
 
 url = "https://raw.githubusercontent.com/sledilnik/data/master/csv/stats.csv"
@@ -13,25 +14,45 @@ rowUmrli = 29
 rowOpravljeni = 4
 
 
-def GetCases():
+def GetCases(arg=None):
+    global rowDan
     csvArray = []
+    rowDan = -2
 
+    # Branje CSV
     with closing(requests.get(url, stream=True)) as r:
+        count = 0
         f = (line.decode('utf-8') for line in r.iter_lines())
         reader = csv.reader(f, delimiter=',', quotechar='"')
         for row in reader:
-            vrstica = [row[rowDatum], row[rowOkuzeni], row[rowSprejetiVBol], row[rowOpravljeni], row[rowUmrli]]
+            vrstica = [row[rowDatum], row[rowOkuzeni], row[rowSprejetiVBol], row[rowOpravljeni], row[rowUmrli], count]
             csvArray.append(vrstica)
+            count += 1
 
-    datetime_obj = datetime.datetime.strptime(csvArray[-2][0], "%Y-%m-%d")
-    weekDay = langDay.GetWeekDay(datetime_obj.strftime("%A"))
+    # Računanje dneva & obdelovanje argumenta
 
-    varBolnica = str(abs(int(csvArray[-2][2]) - int(csvArray[-3][2])))
-    varUmrli = str(abs(int(csvArray[-2][4]) - int(csvArray[-3][4])))
+    if arg is not None:
+        dFormatArg = "%d.%m.%Y"
+        dFormatCSV = "%Y-%m-%d"
+        dTarget = datetime.datetime.strptime(arg, dFormatArg)
+        dStart = datetime.datetime.strptime(csvArray[-2][0], dFormatCSV)
+        indexTarget = csvArray[-2][5] - (dStart - dTarget).days
+        rowDan = indexTarget
+    # Izvleček podatkov & računanje
+    try:
+        datetime_obj = datetime.datetime.strptime(csvArray[rowDan][0], "%Y-%m-%d")
+        varWeekDay = langDay.GetWeekDay(datetime_obj.strftime("%A"))
+        varBolnica = str(abs(int(csvArray[rowDan][2]) - int(csvArray[rowDan-1][2])))
+        varUmrli = str(abs(int(csvArray[rowDan][4]) - int(csvArray[rowDan-1][4])))
+        varOpravljeniTesti = csvArray[rowDan][3]
+        varPotrjeni = csvArray[rowDan][1]
+        # Oblikovanje sporočila
+        msg = "**" + datetime_obj.strftime(varWeekDay + ", %d. %m. %Y") + "**" + \
+              "\nOpravljeni testi: " + "**" + varOpravljeniTesti + "**\n" \
+                                                                   "Potrjeni primeri: " + "**" + varPotrjeni + "**\n" + \
+              "Sprejeti v bolnišnico: " + "**" + varBolnica + "**\n" + \
+              "Umrli: " + "**" + varUmrli + "**"
+    except IndexError:
+        msg = "Nimam podatke ob podanem datumu."
 
-    msg = "**" + datetime_obj.strftime(weekDay + ", %d. %m %Y") + "**" +\
-          "\nOpravljeni testi: " + "**" + csvArray[-2][3] + "**\n"\
-          "Potrjeni primeri: " + "**"+csvArray[-2][1]+"**\n"+\
-          "Sprejeti v bolnišnico: " + "**" + varBolnica + "**\n"+\
-          "Umrli: " + "**" + varUmrli + "**"
     return msg
